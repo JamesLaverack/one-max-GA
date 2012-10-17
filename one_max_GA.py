a6dc1953d244b7fbbe3c4c8fd0e4b02be438f5f4
James Laverack <neoncode@gmail.com>
2012-10-17 16:30:54 +0100

    Adhered to PEP8

--- one_max_GA.py
+++ one_max_GA.py
@@ -8,7 +8,7 @@ def randTrueFalse():
     else:
         return False
 
-        
+
 def weighted_choice(elements, weights):
     value = random.randint(0, sum(weights) - 1)
     # Loop for each element
@@ -18,11 +18,12 @@ def weighted_choice(elements, weights):
             return elements[i]
         else:
             value -= weights[i]
-            
-        
 
-        
-        
+
+def average(L):
+    sum(L) / float(len(L))
+
+
 class one_max_GA:
     '''Performs the 1-max problem optimisation with a genetic algorithm'''
 
@@ -30,43 +31,46 @@ class one_max_GA:
         '''Assigns member variables'''
         self.chromosome_length = chromosome_length
         self.population_size = population_size
-        self.mutation_rate = mutation_rate   
+        self.mutation_rate = mutation_rate
         self.next_population = []
 
     def starting_population(self):
         '''Creates a randomised starting population'''
-        self.population = [[randTrueFalse() for y in range(self.chromosome_length)] for x in range(self.population_size)]
+        self.population = [[randTrueFalse()
+        for y in range(self.chromosome_length)]
+        for x in range(self.population_size)]
         self.fitness = self.measure_fitness()
-        
+
     def measure_fitness(self):
         '''Measures the fitness of each element in the population.
         In this case this means how many True values it contains.'''
-        return [self.population[i].count(True)+1 for i in range(self.population_size)]
-        
+        return [self.population[i].count(True) + 1
+        for i in range(self.population_size)]
+
     def crossover(self, mother, father):
         '''Takes in two parents, performs a crossover operation
         and returns a list of a child and the contrapositive child'''
-        split_point = random.randint(0, self.population_size-1)
+        split_point = random.randint(0, self.population_size - 1)
         child = mother[:split_point] + father[split_point:]
         other_child = mother[split_point:] + father[:split_point]
         return (child, other_child)
-        
+
     def create_children(self):
         mother = weighted_choice(self.population, self.fitness)
         father = weighted_choice(self.population, self.fitness)
         children = self.crossover(mother, father)
         self.next_population += children
-        
+
     def cull(self):
         self.population = self.next_population
         self.next_population = []
         self.fitness = self.measure_fitness()
-        
+
     def do_generation(self):
         while len(self.next_population) < self.population_size:
             self.create_children()
         self.cull()
-        
+
 if __name__ == "__main__":
     GA = one_max_GA(5, 4, 0.25)
     GA.starting_population()
@@ -74,7 +78,6 @@ if __name__ == "__main__":
     #print weighted_choice(GA.population, GA.measure_fitness())
     for i in range(10):
         print "At generation " + str(i)
-        print "Average fitness is " + str(sum(GA.fitness) / float(len(GA.fitness)))
+        print "Average fitness is " + str(average(GA.fitness))
         #print GA.population
         GA.do_generation()
-        
